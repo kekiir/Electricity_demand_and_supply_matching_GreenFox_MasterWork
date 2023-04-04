e135370be3b2c0343f3a35f716564b6b3ae6c5a6
Kéki Szilveszter <kekiir@gmail.com>
2023-04-04 19:42:10 +0200

    startup spring

---
+++ src/test/java/com/gfa/powerTrade/PowerTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.powerTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
