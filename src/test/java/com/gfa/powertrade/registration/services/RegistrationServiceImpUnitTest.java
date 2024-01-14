7493f18002c0f091973f684df3139032313faa70
Kéki Szilveszter <kekiir@gmail.com>
2024-01-14 21:12:24 +0100

    tests for CheckUserNameIsAlreadyTaken method, created it three scenarios.

--- src/test/java/com/gfa/powertrade/registration/services/RegistrationServiceImpUnitTest.java
+++ src/test/java/com/gfa/powertrade/registration/services/RegistrationServiceImpUnitTest.java
@@ -1,17 +1,23 @@
 package com.gfa.powertrade.registration.services;
 
+import com.gfa.powertrade.consumers.models.Consumer;
 import com.gfa.powertrade.consumers.repositories.ConsumerRepository;
+import com.gfa.powertrade.registration.exceptions.AlreadyTakenUsernameException;
 import com.gfa.powertrade.registration.exceptions.InvalidPasswordException;
 import com.gfa.powertrade.registration.models.RegistrationRequestDTO;
+import com.gfa.powertrade.supplier.models.Supplier;
 import com.gfa.powertrade.supplier.repository.SupplierRepository;
 import com.gfa.powertrade.user.services.UserService;
 import com.gfa.powertrade.user.services.UserServiceImp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class RegistrationServiceImpUnitTest {
 
@@ -47,13 +53,51 @@ class RegistrationServiceImpUnitTest {
   }
 
   @Test
-  void checkUsernameIsAlredyTaken() {
+  void checkUsernameIsAlredyTaken_WhenOtherSupplierIsExistingWithThatName() {
 
     // Arrange
+    RegistrationRequestDTO regSupplierWithOccupiedName =
+      new RegistrationRequestDTO("existingSupplierName", null, null);
+    when(supplierRepository.findByUsername("existingSupplierName")).thenReturn(Optional.of(new Supplier()));
 
     // Act
 
     // Assert
+    assertThrows(AlreadyTakenUsernameException.class,
+      () -> registrationService.checkUsernameIsAlredyTaken(regSupplierWithOccupiedName));
+  }
+
+  @Test
+  void checkUsernameIsAlredyTaken_WhenOtherConsumerIsExistingWithThatName() {
+
+    // Arrange
+    RegistrationRequestDTO regConsumerWithOccupiedName =
+      new RegistrationRequestDTO("existingConsumerName", null, null);
+    when(consumerRepository.findByUsername("existingConsumerName")).thenReturn(Optional.of(new Consumer()));
+    // Act
+
+    // Assert
+    assertThrows(AlreadyTakenUsernameException.class,
+      () -> registrationService.checkUsernameIsAlredyTaken(regConsumerWithOccupiedName));
+  }
+
+  @Test
+  void checkUsernameIsAlredyTaken_WhenUsernameIsFree() {
+
+    // Arrange
+    RegistrationRequestDTO regSupplierWithFreedName =
+      new RegistrationRequestDTO("FreeSupplierName", null, null);
+    RegistrationRequestDTO regConsumerWithFreedName =
+      new RegistrationRequestDTO("FreeConsumerName", null, null);
+
+    when(supplierRepository.findByUsername("FreeSupplierName")).thenReturn(Optional.empty());
+    when(supplierRepository.findByUsername("FreeConsumerName")).thenReturn(Optional.empty());
+
+    // Act
+
+    // Assert
+    assertDoesNotThrow(() -> registrationService.checkUsernameIsAlredyTaken(regSupplierWithFreedName));
+    assertDoesNotThrow(() -> registrationService.checkUsernameIsAlredyTaken(regConsumerWithFreedName));
   }
 
   @Test
