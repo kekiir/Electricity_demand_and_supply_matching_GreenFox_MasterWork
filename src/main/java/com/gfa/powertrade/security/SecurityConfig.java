1d0666cca49fdb79077c614f1c58e030971db526
Kéki Szilveszter <kekiir@gmail.com>
2024-10-01 22:36:46 +0200

    frontend acces the /messages endpoint from frontend

--- src/main/java/com/gfa/powertrade/security/SecurityConfig.java
+++ src/main/java/com/gfa/powertrade/security/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
   private CorsConfiguration setCorsConfig () {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("http://localhost:5173");
+    config.addAllowedOrigin("http://localhost:3000");
     config.setAllowedHeaders(Arrays.asList(
       HttpHeaders.AUTHORIZATION,
       HttpHeaders.CONTENT_TYPE,
